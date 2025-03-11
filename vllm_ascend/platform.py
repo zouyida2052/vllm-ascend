dfb4e23e9d820ac992a071c123bbe983c7b01b2e
Tony <864832769@qq.com>
2025-03-11 19:14:14 +0800

    0.7.3 multi step no tests (#300)
    Add multi step scheduler support for vllm-ascend
    
    Signed-off-by: new-TonyWang <wangtonyyu222@gmail.com>

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -111,9 +111,13 @@ class NPUPlatform(Platform):
             if vllm_config.speculative_config:
                 parallel_config.worker_cls = "vllm.spec_decode.spec_decode_worker.create_spec_worker"
                 parallel_config.sd_worker_cls = "vllm_ascend.worker.worker.NPUWorker"
+            elif vllm_config.scheduler_config.is_multi_step:
+                parallel_config.worker_cls = "vllm_ascend.worker.multi_step_worker.MultiStepWorker"
             else:
                 parallel_config.worker_cls = "vllm_ascend.worker.worker.NPUWorker"
+
         cache_config = vllm_config.cache_config
+        vllm_config.scheduler_config.chunked_prefill_enabled = False
         if cache_config and cache_config.block_size is None:
             cache_config.block_size = 128
         if vllm_config.quant_config is not None and \

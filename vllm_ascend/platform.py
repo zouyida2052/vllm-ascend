176b0d694dd08d4b698fa891d510993f29a7e5b4
Shanshan Shen <467638484@qq.com>
2025-04-02 14:18:00 +0800

    [Bugfix] Fix `Invalid device id` error for V1 when using multi discontinuous NPUs (#452)
    ### What this PR does / why we need it?
    Fix `Invalid device id` error for V1 when using multi discontinuous
    NPUs.
    
    Find more details at
    https://github.com/vllm-project/vllm-ascend/issues/449.
    
    
    Signed-off-by: shen-shanshan <467638484@qq.com>

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -49,19 +49,6 @@ os.environ["RAY_EXPERIMENTAL_NOSET_ASCEND_RT_VISIBLE_DEVICES"] = "1"
 logger = init_logger(__name__)
 
 
-def _device_id_to_physical_device_id(device_id: int) -> int:
-    if "ASCEND_RT_VISIBLE_DEVICES" in os.environ:
-        device_ids = os.environ["ASCEND_RT_VISIBLE_DEVICES"].split(",")
-        if device_ids == [""]:
-            raise RuntimeError("ASCEND_RT_VISIBLE_DEVICES is set to empty"
-                               "string, which means Ascend NPU support is"
-                               "disabled.")
-        physical_device_id = device_ids[device_id]
-        return int(physical_device_id)
-    else:
-        return device_id
-
-
 class NPUPlatform(Platform):
 
     _enum = PlatformEnum.OOT
@@ -87,8 +74,7 @@ class NPUPlatform(Platform):
 
     @classmethod
     def get_device_name(cls, device_id: int = 0) -> str:
-        physical_device_id = _device_id_to_physical_device_id(device_id)
-        return torch.npu.get_device_name(physical_device_id)
+        return torch.npu.get_device_name(device_id)
 
     @classmethod
     def is_async_output_supported(cls, enforce_eager: Optional[bool]) -> bool:

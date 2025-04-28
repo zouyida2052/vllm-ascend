4ed7e969a54c4e5a0edcb53c017930c4bf35b988
zouyida2052 <zouyida2002@gmail.com>
2025-04-28 17:03:01 +0800

    cleancode for qwen2_vl
    Signed-off-by: zouyida2052 <zouyida2002@gmail.com>

--- vllm_ascend/models/qwen2_vl.py
+++ vllm_ascend/models/qwen2_vl.py
@@ -143,11 +143,15 @@ class AscendQwen2VisionBlock(Qwen2VisionBlock):
         sin: torch.Tensor,
     ) -> torch.Tensor:
         x = x + self.attn(
-            self.norm1(x), cu_seqlens=cu_seqlens, cos=cos, sin=sin,
+            self.norm1(x),
+            cu_seqlens=cu_seqlens,
+            cos=cos,
+            sin=sin,
         )
 
         x = x + self.mlp(self.norm2(x))
         return x
+
 
 class AscendQwen2VisionPatchEmbed(Qwen2VisionPatchEmbed):
 

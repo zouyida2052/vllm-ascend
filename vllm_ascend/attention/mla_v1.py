b6aa5bbdbf342867695b58e8163638abcba6d486
Zetong Li <48438720+slippersss@users.noreply.github.com>
2026-04-15 09:23:52 +0800

    [0.18.0][BugFix] Add PrefillNoCache state in mla _forward_decode for short prompt (#8264)
    ### What this PR does / why we need it?
    This PR is cherry-pick from #8263.
    
    This PR aims to fix short prompt problem. The root cause can be found in
    #8029. Since the previous pr may miss mixed long and short prompt batch,
    after discussion, we decide to add PrefillNoCache state in mla
    _forward_decode now instead.
    
    Signed-off-by: Zetong Li <slippersss@126.com>

--- vllm_ascend/attention/mla_v1.py
+++ vllm_ascend/attention/mla_v1.py
@@ -1270,6 +1270,7 @@ class AscendMLAImpl(MLAAttentionImpl):
                 AscendAttentionState.SpecDecoding,
                 AscendAttentionState.ChunkedPrefill,
                 AscendAttentionState.DecodeOnly,
+                AscendAttentionState.PrefillNoCache,  # for extremely short prefills
             ]
             and self.speculative_config is not None
         ):

dd7e08c6db5f777933db3426c514d31d41bb20ef
zyz111222 <zouyizhou@huawei.com>
2026-04-20 17:20:40 +0800

    [Performance] Use forward_native for Conv3dLayer and add UT (#8375)
    What this PR does / why we need it?
    switch Ascend conv3d forward_oot to use forward_native and  add ut
    
    Does this PR introduce any user-facing change?
    No
    
    How was this patch tested?
    by CI
    
    ---------
    
    Signed-off-by: zouyizhou <zouyizhou@huawei.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -662,6 +662,7 @@ def register_ascend_customop(vllm_config: VllmConfig | None = None):
     if is_310p():
         from vllm_ascend._310p.fused_moe.fused_moe import AscendFusedMoE310, AscendSharedFusedMoE310
         from vllm_ascend._310p.ops.activation import AscendSiluAndMul310
+        from vllm_ascend._310p.ops.conv import AscendConv3dLayer310
         from vllm_ascend._310p.ops.layernorm import (
             AscendGemmaRMSNorm310,
             AscendRMSNorm310,
@@ -686,6 +687,7 @@ def register_ascend_customop(vllm_config: VllmConfig | None = None):
                 "ParallelLMHead": AscendParallelLMHead310,
                 "VocabParallelEmbedding": AscendVocabParallelEmbedding310,
                 "MMEncoderAttention": AscendMMEncoderAttention310,
+                "Conv3dLayer": AscendConv3dLayer310,
             }
         )
 
